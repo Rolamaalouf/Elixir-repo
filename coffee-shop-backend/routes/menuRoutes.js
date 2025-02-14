d6a8a974c205a99472d191ef80de28ce9e228240
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-02-14 15:28:30 +0200

    menu backend minor rectifications

--- coffee-shop-backend/routes/menuRoutes.js
+++ coffee-shop-backend/routes/menuRoutes.js
@@ -5,8 +5,15 @@ const menuController = require("../controllers/menuController");
 // Define routes
 router.get("/", menuController.getMenu);
 router.get("/:id", menuController.getMenuById);
+router.get("/section/:sectionId", menuController.getMenuSectionById);
 router.post("/", menuController.createMenu);
-router.put("/:id", menuController.updateMenu);
+router.put("/", menuController.updateMenu);
 router.delete("/:id", menuController.deleteMenu);
+router.patch("/:category/:itemId", menuController.updateItemByCategoryAndId);
+
+// Retrieves an item (subcategory) within a specific category
+router.get("/:category/:itemId", menuController.getItemByCategoryAndId);
+
+
 
 module.exports = router;
